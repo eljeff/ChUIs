fbb60eeed22370e8e763086db9503ce10e011e7b
Jeff Cooper <jeffocooper@gmail.com>
2021-12-21 12:01:03 -0600

    slider responds to movement

--- Sources/ChUIs/Sliders/ColourSlider.swift
+++ Sources/ChUIs/Sliders/ColourSlider.swift
@@ -18,6 +18,8 @@ open class ColourSlider: UIControl {
 
     private let trackLayer = CALayer()
     private let thumbImageView = UIImageView()
+    
+    private var previousLocation = CGPoint()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,28 +31,82 @@ open class ColourSlider: UIControl {
         setupViews()
     }
     
+    open override var frame: CGRect {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    open override func layoutSubviews() {
+        updateLayerFrames()
+    }
+    
     private func setupViews() {
         trackLayer.backgroundColor = UIColor.blue.cgColor
         layer.addSublayer(trackLayer)
         
         thumbImageView.image = thumbImage
         addSubview(thumbImageView)
+        
+        updateLayerFrames()
     }
     
-    // 1
     private func updateLayerFrames() {
-      trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height / 3)
-      trackLayer.setNeedsDisplay()
-        thumbImageView.frame = CGRect(origin: thumbOriginForValue(value),
-                                         size: thumbImage.size)
+        trackLayer.frame = bounds.insetBy(dx: bounds.width / 3, dy: 0)
+        trackLayer.setNeedsDisplay()
+        thumbImageView.frame = CGRect(origin: thumbOriginForValue(value), size: thumbImage.size)
     }
-    // 2
+    
     func positionForValue(_ value: CGFloat) -> CGFloat {
-      return bounds.width * value
+        return bounds.height * value
     }
-    // 3
+    
     private func thumbOriginForValue(_ value: CGFloat) -> CGPoint {
-      let x = positionForValue(value) - thumbImage.size.width / 2.0
-      return CGPoint(x: x, y: (bounds.height - thumbImage.size.height) / 2.0)
+        let y = positionForValue(value) - thumbImage.size.height / 2.0
+        return CGPoint(x: (bounds.width - thumbImage.size.width) / 2.0, y: y)
     }
+}
+
+extension ColourSlider {
+    
+    open override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        
+        previousLocation = touch.location(in: self)
+        
+        if thumbImageView.frame.contains(previousLocation) {
+            thumbImageView.isHighlighted = true
+        }
+        
+        return thumbImageView.isHighlighted
+    }
+    
+    open override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let location = touch.location(in: self)
+        let deltaLocation = location.y - previousLocation.y
+        let deltaValue = (maximumValue - minimumValue) * deltaLocation / bounds.height
+        
+        previousLocation = location
+        
+        if thumbImageView.isHighlighted {
+            value += deltaValue
+            value = boundValue(value, toLowerValue: minimumValue,
+                               upperValue: maximumValue)
+        }
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        updateLayerFrames()
+        CATransaction.commit()
+        return true
+    }
+    
+    private func boundValue(_ value: CGFloat, toLowerValue lowerValue: CGFloat,
+                            upperValue: CGFloat) -> CGFloat {
+        return min(max(value, lowerValue), upperValue)
+    }
+    
+    open override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+      thumbImageView.isHighlighted = false
+    }
+
 }
